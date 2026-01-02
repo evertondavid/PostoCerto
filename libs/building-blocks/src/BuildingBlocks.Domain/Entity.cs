51b7b7800488888140105539dc7271638bf96b5c
Everton David <contato.evertondavid@gmail.com>
2026-01-02 17:17:38 -0400

    feat(building-blocks): add Entity<TId> base class with equality implementation

---
+++ libs/building-blocks/src/BuildingBlocks.Domain/Entity.cs
@@ -0,0 +1,36 @@
+﻿namespace BuildingBlocks.Domain;
+
+public abstract class Entity<TId> where TId : notnull
+{
+    public TId Id { get; protected set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+    
+    // Protected constructor for EF Core
+    protected Entity() { 
+      Id = default!; // Default value for TId
+    }
+
+     public override bool Equals(object? obj)
+    {
+        if (obj is not Entity<TId> other)
+            return false;
+       
+       if(ReferenceEquals(this, other))
+            return true;
+      
+        return Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
